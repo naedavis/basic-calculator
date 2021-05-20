7faa1fd8446c1b43eb8a12a4d855d707cb342df0
naedavis <naeemahndavis@gmail.com>
2021-05-20 15:56:28 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+#basic calculator
+
+class Add:
+    def __init__(self):
+        pass
+    def add(self, a, b):
+        result= a+b
+
+
+class Subtract:
+    def __init__(self):
+        pass
+    def add(self, a, b):
+        result= a-b
+
+
+class Multiply:
+    def __init__(self):
+        pass
+    def add(self, a, b):
+        result= a*b
+
+
+class Divide:
+    def __init__(self):
+        pass
+    def add(self, a, b):
+        result= a/b
